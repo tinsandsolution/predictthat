e16a02fd696512052a0cc3017f447f4f1bc2325f
Standard <oanlara8@gmail.com>
2022-10-25 16:18:22 -0600

    initial migration

--- migrations/versions/migrationfile.py
+++ migrations/versions/migrationfile.py
@@ -1,8 +1,8 @@
-"""create_users_table
+"""empty message
 
-Revision ID: ffdc0a98111c
+Revision ID: 219a9b6f3b09
 Revises: 
-Create Date: 2020-11-20 15:06:02.230689
+Create Date: 2022-10-25 16:17:55.445211
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ffdc0a98111c'
+revision = '219a9b6f3b09'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -27,7 +27,7 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
-    # ### end Alembic commands ###qqqqqqqqq
+    # ### end Alembic commands ###
 
 
 def downgrade():
