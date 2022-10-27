9d5d028812d2f8bf33a89556716b0112c9e8706b
Standard <oanlara8@gmail.com>
2022-10-27 15:01:06 -0600

    create shares form

--- react-app/src/components/SingleMarketPage/CreateSharesModal/CreateSharesForm.js
+++ react-app/src/components/SingleMarketPage/CreateSharesModal/CreateSharesForm.js
@@ -5,110 +5,62 @@ import { createMarket } from '../../../store/market';
 
 const CreateSharesForm = ({setShowModal}) => {
   const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
   const history = useHistory();
 
   const [errors, setErrors] = useState([]);
+  const [pairs, setPairs] = useState(1)
 
-  // title
-  const [title, setTitle] = useState('')
-  // short_title
-  const [short_title, setShort_title] = useState('')
-  // image_url
-  const [image_url, setImage_url] = useState('')
-  // description
-  const [description, setDescription] = useState('')
-  // date needs filling
-
-  // const user = useSelector(state => state.session.user);
   const onSubmitMarket = async (e) => {
     e.preventDefault();
     let errors = [];
-    // title must be between 10 and 100 characters
-    if (title.length < 10 || title.length > 100) errors.push("Title must be between 10 and 100 characters.")
-    // short title must be between 5 and 25 characters
-    if (short_title.length < 5 || short_title.length > 25) errors.push("Short title must be between 5 and 25 characters.")
-    // description must be between 10 characters and 250 characters
-    if (description.length < 10 || description.length > 250) errors.push("Description must be between 10 and 250 characters.")
-    // image url must end with a .jpg, a .jpeg, or a .png
-    const imgRegex = /.*\.(jpg|png|jpeg)$/
-    if (!image_url.match(imgRegex)) errors.push("Image must end with a .jpg, a .jpeg, or a .png")
+    if (parseInt(pairs) <= 0) errors.push("You cannot create less than one pair.")
+    if (parseInt(pairs) > Math.floor(parseInt(sessionUser.funds))) errors.push("You do not have enough money in your account.")
 
     if (errors.length) setErrors(errors)
-
-    //
     else {
-      const data = await dispatch(createMarket({title, short_title, description, image_url}));
+      // const data = await dispatch(createMarket({title, short_title, description, image_url}));
       setShowModal(false)
-      return history.push('/yourmarkets')
+      // return history.push('/yourmarkets')
     }
   };
 
-  const updateTitle = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const updateShort_title = (e) => {
-    setShort_title(e.target.value);
-  };
-
-  const updateImage_url = (e) => {
-    setImage_url(e.target.value);
-  };
-
-  const updateDescription = (e) => {
-    setDescription(e.target.value);
+  const updatePairs = (e) => {
+    setPairs(e.target.value);
   };
 
 
   return (
-    <form className="modal-form create-market-modal-form" onSubmit={onSubmitMarket}>
-      <div className='form-title'> Create a Market </div>
+    <form className="modal-form create-shares-modal-form" onSubmit={onSubmitMarket}>
+      <div className='form-title'> Create Pair Shares </div>
+      <div className='form-explanation'>
+        When you create a pair of shares:
+        <ul>
+          <li>$1 is withdrawn from your account</li>
+          <li>You are credited with one "yes" and one "no" share.</li>
+        </ul>
+        Afterwards, you are able list those shares on the market at whatever price you would like.
+      </div>
       <div className='modal-errors'>
         {errors.map((error, ind) => (
           <div className="modal-form-error" key={ind}>{error}</div>
         ))}
       </div>
       <div className='form-single-data'>
-        <label htmlFor='title'>Market Title </label>
+        <label htmlFor='pairs'>Pair Shares To Create - {"Max. " + Math.floor(parseInt(sessionUser.funds)) +" Shares"}</label>
         <input
-          name='title'
-          type='text'
-          placeholder='Will Oprah Run For President In 2028?'
-          value={title}
-          onChange={updateTitle}
+          name='pairs'
+          type='number'
+          min="1"
+          max={Math.floor(parseInt(sessionUser.funds))}
+          // max="100"
+          placeholder=""
+          value={pairs}
+          onChange={updatePairs}
         />
       </div>
-      <div className='form-single-data'>
-        <label htmlFor='short_title'>Short Title</label>
-        <input
-          name='short_title'
-          type='text'
-          placeholder='Oprah 2028'
-          value={short_title}
-          onChange={updateShort_title}
-        />
-      </div>
-      <div className='form-single-data'>
-        <label htmlFor='image_url'>Image Url</label>
-        <input
-          name='image_url'
-          type='text'
-          placeholder='https://i.imgur.com/jG3tSug.png'
-          value={image_url}
-          onChange={updateImage_url}
-        />
-      </div>
-      <div className='form-single-data'>
-        <label htmlFor='description'>Description</label>
-        <input
-          name='description'
-          type='text'
-          placeholder='The WSJ will be used as the defining authority to determine the outcome.'
-          value={description}
-          onChange={updateDescription}
-        />
-      </div>
-      <button className="black-button button-margin" type='submit'>Create Market</button>
+
+      <button className="black-button button-margin" type='submit'>Confirm Transaction</button>
     </form>
   );
 };
