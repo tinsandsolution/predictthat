a0e6a7acf5aed0329fb9edcda0df7b7c8d26eedc
Standard <oanlara8@gmail.com>
2022-10-26 19:41:55 -0600

    note to self never use viewwidth

--- react-app/src/components/HomePage/LoggedInPage.js
+++ react-app/src/components/HomePage/LoggedInPage.js
@@ -7,6 +7,49 @@ const LoggedInPage = () => {
     markets.sort((a,b) => a["id"] - b["id"])
 
     let featureMarkets = markets.slice(0,4)
+    let restMarkets = markets.slice(4)
+    // restMarkets
+
+    const makeRegularCards = (markets) => {
+        return (
+            <>
+            {markets.map(market => {
+
+                let yesno = (
+                    <>
+                        <div className='splash-yes'> Yes {market.yes_value}¢</div>
+                        <div className='splash-no'> No {market.no_value}¢</div>
+                    </>
+                )
+
+                if (market.yes_value === 0 && market.no_value === 0) {
+                    yesno = (
+                        <div className="featured-none-at-all">No Bets Yet</div>
+                    )
+                }
+
+                return (
+                    <div className='rest-card-wrapper'>
+                    <div className='rest-card'>
+                        <div className="rest-card-left">
+                            <div className='rest-card-title'> {market["short_title"]} </div>
+                            <div className='featured-card-resolves'> {market.expected_resolution_time === null ? "Ongoing" : market.expected_resolution_time} </div>
+                            <div className='featured-yes-no'>
+                                {yesno}
+                            </div>
+                        </div>
+                        <div className="rest-card-right">
+                            <img className='rest-card-image' alt="" src={market["image_url"]}></img>
+                        </div>
+                     </div>
+                    </div>
+                )
+
+            })}
+            </>
+        )
+    }
+
 
     const makeFeaturedCards = (markets) => {
         return (
@@ -48,11 +91,19 @@ const LoggedInPage = () => {
 
     return (
         <div className="logged-in-container">
-            <div className="logged-in-header">
-                Featured Markets
-                <div className="featured-markets">
-                    {makeFeaturedCards(featureMarkets)}
+            <div>
+                <div className="logged-in-header">
+                    Featured Markets
                 </div>
+            </div>
+            <div className="featured-markets">
+                    {makeFeaturedCards(featureMarkets)}
+            </div>
+            <div className="logged-in-header2">
+                Recently Added
+            </div>
+            <div className="rest-markets">
+                    {makeRegularCards(restMarkets)}
             </div>
         </div>
     )
