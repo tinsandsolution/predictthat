5ad5af7a428c6680736a02e4213247dc15f21652
Standard <oanlara8@gmail.com>
2022-12-09 16:43:55 -0700

    finished the filtering, now to just display

--- react-app/src/components/Search/Search.js
+++ react-app/src/components/Search/Search.js
@@ -2,12 +2,31 @@ import { connect, useSelector } from "react-redux"
 import { useLocation, withRouter } from "react-router-dom"
 
 
+const filterMarkets = (terms, markets) => {
+    terms = terms.map(term => term.toLowerCase())
+
+    return markets.filter(market => {
+        for (let term of terms) {
+            let nonalpha = /[^A-Za-z0-9]/g
+            let marketWords = [...market.description.split(" "),...market.title.split(" ")]
+            marketWords = marketWords.map(marketWord => marketWord.replace(nonalpha, "").toLowerCase())
+
+            if (marketWords.includes(term)) return true
+        }
+        return false
+    })
+}
+
+
+
 const Search = () => {
     const { search } = useLocation();
     const raw = String(new URLSearchParams(search)).slice(2)
     const terms = raw.split("+")
     let markets = useSelector((state) => state.markets)
 
+    const filteredMarkets = filterMarkets(terms, markets)
+    console.log(filteredMarkets)
     return (
         <>
         {terms[0]}
